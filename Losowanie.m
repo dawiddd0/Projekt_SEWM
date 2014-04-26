6f6d3dcd138e60062ae9b962f7441eb0cc8afae3
Klaudia <kllaudiaa@gmail.com>
2014-04-26 17:17:53 +0200

    Początek
    Koder

---
+++ Losowanie.m
@@ -0,0 +1,54 @@
+%wybranie 10 losowych bitów
+
+function [ RandX ] = Losowanie(  )
+seed = 12356;  % ziarno
+step = 8;
+RandX = zeros(step);
+ilosc = 0;
+while ilosc < 6
+    x = randi(step);
+    y = randi(step);
+    if RandX(x,y) ~= 1
+        RandX(x,y) = 1;
+        ilosc = ilosc + 1;
+    end
+end
+ilosc = 0;
+while ilosc < 1
+    x = randi(step/2);
+    y = randi(step/2);
+    if RandX(x,y) == 0
+        RandX(x,y) = 2;
+        ilosc = ilosc + 1;
+    end
+end
+ilosc = 0;
+while ilosc < 1
+    x = randi(step/2);
+    y = randi(step/2)+4;
+    if RandX(x,y) == 0
+        RandX(x,y) = 2;
+        ilosc = ilosc + 1;
+    end
+end
+ilosc = 0;
+while ilosc < 1
+    x = randi(step/2)+4;
+    y = randi(step/2);
+    if RandX(x,y) == 0
+        RandX(x,y) = 2;
+        ilosc = ilosc + 1;
+    end
+end
+ilosc = 0;
+while ilosc < 1
+    x = randi(step/2)+4;
+    y = randi(step/2)+4;
+    if RandX(x,y) == 0
+        RandX(x,y) = 2;
+        ilosc = ilosc + 1;
+    end
+end
+RandX;
+end
+
