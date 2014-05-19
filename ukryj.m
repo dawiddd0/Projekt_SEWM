ccbe7c4f69f80e9fa2c2878d2c3cc5be80a47dae
dawiddd0 <ddjrym@student.agh.edu.pl>
2014-05-19 21:31:02 +0200

    udoskonalenie algorytmu - jasnosc

--- ukryj.m
+++ ukryj.m
@@ -1,20 +1,21 @@
 function [pic1,macierz]=ukryj(im)
 im=rgb2gray(im);
+im=jasnosc(im);
 wim=dither(im); %dithering
 LPLP=dwt2((wim),'db1'); %dekompozycja falkowa
 [m,n]=size(LPLP);
 macierz=zeros(m,n);
-for i=1:256*256         %generowanie losowej macierz 0 i 1
-    j=uint8(rand(1)*255)+1;
-    k=uint8(rand(1)*255)+1;    
+for i=1:m*n         %generowanie losowej macierz 0 i 1
+    j=uint8(rand(1)*(m-1))+1;
+    k=uint8(rand(1)*(n-1))+1;    
     macierz(j,k)=1;    
 end
 pic=xor((LPLP),(macierz)); %XOR macierzy i dolnoprzepustowej sk³adowej dekompozycji falkowej
-k=randperm(256); %przygotowanie permutacji
-l=randperm(256);
-picperm=zeros(256,256);
-for i=1:256     %wykonanie permutacji
-    for j=1:256
+k=randperm(m); %przygotowanie permutacji
+l=randperm(n);
+picperm=zeros(m,n);
+for i=1:m     %wykonanie permutacji
+    for j=1:n
         picperm(k(i),l(j))=pic(i,j);
     end
 end
@@ -34,8 +35,8 @@ picperm(123,:)=0;
 picperm(122,:)=0;
 picperm(121,:)=0;
 pic1=zeros(256,256);
-for i=1:256     %u³o¿enie bitów w odpowiedniej kolejnoœci (odwrotna permutacja)
-    for j=1:256
+for i=1:m     %u³o¿enie bitów w odpowiedniej kolejnoœci (odwrotna permutacja)
+    for j=1:n
         pic1(i,j)=picperm(k(i),l(j));
     end
 end
@@ -45,3 +46,17 @@ imshow(pic1);   %obraz wynikowy z rekonustrukcji otrzymany
 
 
 imwrite(pic1,'rekonstrukcja.jpg','jpg'); %zapisanie obrazu zdekodowanego
+
+
+function [pic]=jasnosc(im)
+im=im-(mean(mean(im))-255/2); %przeniesienie w œrodek czêstotliwoœci
+%rzutowanie na przedzia³
+imm=im*(255/(max(max(im))*(max(max(im))-min(min(im)))));
+imm=double(imm);
+maxi=double(max(max(im)));
+mini=double(min(min(im)));
+pic=(imm)*(255/(maxi-mini));
+mini=min(min(pic));
+pic=pic-mini;
+pic=uint8(pic);
+
