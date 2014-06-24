909526970ca24c65841dbc5bd0456a44063396e1
Klaudia <kllaudiaa@gmail.com>
2014-06-24 17:53:21 +0200

    DZIAŁA

--- ckuf.m
+++ ckuf.m
@@ -1,7 +1,12 @@
 function [pic]=ckuf(file,krok,tryb,save)
+krok=50;
 im=imread(file);
 j='.jpg';
 save=[save j];
+[a,b,c]=size(im);
+if a==3 | b==3 | c==3
+   im=rgb2gray(im); 
+end
 if tryb==0
 [szum]=ukryj(im);
 pic=kodowanie(im,szum,krok);
@@ -10,13 +15,11 @@ imshow(uint8(pic));
 pic=uint8(pic);
 else
 pic=imread(file);
-pic1=dekodowanie(pic,krok).*255;
-w=fspecial('gaussian');
-pic1=imfilter(pic1,w);
+[x,y,z]=size(pic);
+pic1=dekodowanie(pic,krok);
+pic11=imresize(pic1,[x y]);
 [A,V,H,D]=dwt2(pic,'haar');
-pic11=idwt2(pic1,V,H,D,'haar');
-w=fspecial('gaussian');
-pic11=imfilter(pic11,w);
+%pic11=idwt2(pic1,V,H,D,'haar','mode','sym');
     if tryb == 1
 imwrite(uint8(pic11),save,'jpg');
 imshow(uint8(pic11));
@@ -27,118 +30,124 @@ imshow(uint8(pic11)-pic);
 pic=pic11;    
     end
 end
+
 function [pic21]=dekodowanie(im,krok)
 %im=rgb2gray(im);
-[A,V,H,D]=dwt2(im,'haar');
-[m,n]=size(A);
+[A,V,H,D]=dwt2(im,'haar','mode','sym');
+MAX = max(A(:));
+[m,n,k]=size(A);
 szum=zeros(m,n);
-s = RandStream('mt19937ar','Seed',0);
-k=randperm(s,m); %przygotowanie permutacji
-s = RandStream('mt19937ar','Seed',0);
-l=randperm(s,n);
+
+zera=0:2*krok:MAX+krok;
+jedynki=0+krok:2*krok:MAX;
 %%%Odczytywanie znaku wodnego%%%
 for i=1:m 
     for j=1:n
+        
         if (rem(A(i,j),2*krok)>(krok/2) && rem(A(i,j),2*krok)<(krok+krok/2))
             szum(i,j)=1;
         else
             szum(i,j)=0;
-        end        
+        end  
     end
 end
-for i=1:m     %u³o¿enie bitów w odpowiedniej kolejnoœci (odwrotna permutacja)
-    for j=1:n
-        pic1(i,j)=szum(k(i),l(j));
-    end
-end
-macierz=losowanie(m,n);
-pic2=xor(macierz,pic1);
-imwrite((pic2),'odzyskano.jpg','jpg');
-w=fspecial('gaussian');
-pic21=imfilter((pic2),w);
-
-function [pic]=kodowanie(im,szum,krok)
-[a,b,c]=size(im);
-if a==3 | b==3 | c==3
-im=rgb2gray(im);
-end
-[A,V,H,D]=dwt2(im,'haar');
-[m,n]=size(A);
-Aemb=A; %Aemb macierz A, do ktrej zostanie dodany payload
-%%%dodawanie payloadu%%%
-for i=1:m
-    for j=1:n
-        if (rem(A(i,j),(2*krok))>(krok/2) && rem(A(i,j),(2*krok))<(krok+krok/2))
-            Aemb(i,j)=2*krok*round(A(i,j)/(2*krok))+szum(i,j)*krok;
-        else
-            Aemb(i,j)=2*krok*round(A(i,j)/(2*krok))-szum(i,j)*krok;
-        end
-    end
-end
-
-pic=idwt2(double(Aemb),V,H,D,'haar'); %scalenie nowej macierzy A w jeden obrazek
-imshow(uint8(pic));
-imwrite(uint8(pic),'zakodowany.jpg','jpg');
-[psnr] = measerr(uint8(pic),double(pic))
-function [picperm]=ukryj(im)
-[a,b,c]=size(im);
-if a==3 | b==3 | c==3
-im=rgb2ntsc(im);
-im=rgb2gray(im);
-end
-im=dither(im);
-%imshow(im);
-[LPLP,LPHP,HPLP,HPHP]=dwt2(im,'haar'); %dekompozycja falkowa
-%imshow(LPLP);
-[m,n]=size(LPLP);
-macierz=losowanie(m,n);
-pic=xor((LPLP),(macierz)); %XOR macierzy i dolnoprzepustowej sk³adowej dekompozycji falkowej
+szum = logical(szum);
+[m,n,k]=size(szum);
+rng(2,'twister'); 
+macierz = randi([0 1],m,n,'uint8'); 
+macierz=logical(macierz);
+pic2=bitxor(szum, macierz);
+[m,n]=size(szum);
+rng(2,'twister'); 
+pion = randperm(n);
+rng(2,'twister');
+poziom = randperm(m);
 s = RandStream('mt19937ar','Seed',0);
 k=randperm(s,m); %przygotowanie permutacji
 s = RandStream('mt19937ar','Seed',0);
 l=randperm(s,n);
-picperm=zeros(m,n);
+for i=1:m     %u??nie bit? w odpowiedniej kolejno?i (odwrotna permutacja)
+    for j=1:n
+        pic1(i,j)=pic2(k(i),l(j));
+    end
+end
+
+
+
+
+
+pic1=uint8(pic1);
+pic1=pic1.*255;
+imwrite((pic1),'odzyskano.jpg','jpg');
+w=fspecial('gaussian',[5 5],2);
+pic21=imfilter((pic1),w,'same');
+
+imshow(uint8(pic21))
+
+
+function [pic]=kodowanie(im,szum,krok)
+
+[A,V,H,D]=dwt2(im,'haar','mode','sym');
+[a,b,c]=size(im);
+
+
+[m,n,k]=size(szum);
+MAX = max(A(:));
+zera=0:2*krok:MAX+krok;
+jedynki=krok:2*krok:MAX;
+Aemb=A; %Aemb macierz A, do ktrej zostanie dodany payload
+%%%dodawanie payloadu%%%
+for i=1:m
+    for j=1:n
+       if szum(i,j) == 1 
+            [~,I]=min(abs(Aemb(i,j)-jedynki));
+            Aemb(i,j)=jedynki(I);
+        end
+        
+        if szum(i,j) == 0
+            [~,I]=min(abs(Aemb(i,j)-zera));
+            Aemb(i,j)=zera(I);
+        end
+        
+        
+        
+    end
+end
+
+pic=idwt2((Aemb),V,H,D,'haar',[a b]); %scalenie nowej macierzy A w jeden obrazek
+imwrite(uint8(pic),'zakodowany.jpg','jpg');
+[psnr] = measerr(uint8(pic),double(pic))
+function [pic]=ukryj(im)
+
+
+im=dither((im));
+[LPLP]=dwt2(im,'haar','mode','sym'); %dekompozycja falkowa
+
+imshow(LPLP);
+[m, n]=size(LPLP);
+
+
+rng(2,'twister'); 
+pion = randperm(n);
+
+rng(2,'twister');
+poziom = randperm(m);
+
+s = RandStream('mt19937ar','Seed',0);
+k=randperm(s,m); %przygotowanie permutacji
+s = RandStream('mt19937ar','Seed',0);
+l=randperm(s,n);
+pi=zeros(m,n);
 for i=1:m     %wykonanie permutacji
    for j=1:n
-      picperm(k(i),l(j))=pic(i,j);
+      pi(k(i),l(j))=LPLP(i,j);
     end
 end
-imwrite(picperm,'szum.jpg','jpg'); %zapisanie obrazu zakodowanego
-%%%rekonstrukcja
-pic1=zeros(m,n);
-for i=1:m     %u³o¿enie bitów w odpowiedniej kolejnoœci (odwrotna permutacja)
-    for j=1:n
-        pic1(i,j)=picperm(k(i),l(j));
-    end
-end
-pic1=xor(macierz,pic1); % w³aœciwa rekonstrukcja
-%imshow(pic1);   %obraz wynikowy, z rekonustrukcji otrzymany
-imwrite(pic1,'rekonstrukcja.jpg','jpg'); %zapisanie obrazu zdekodowanego
-function [y]=losowanie(h,b)
-y=zeros(h,b);
-m=10113904223;
-a=1664525;
-y(1)=rem(a+m,255);
-for i=2:round(h/4)
-    for j=2:round(b/4)
-    y(i,j)=rem(y(i-1,j-1)*a+m,255);
-    end
-end
+rng(2,'twister'); 
+macierz = randi([0 1],m,n,'uint8'); 
+macierz=uint8(macierz);
 
-for i=round(h/4):round(h/2)
-    for j=round(b/4):round(b/2)
-    y(i,j)=rem(y(i-1,j-1)*a+m,255);
-    end
-end
+pic=bitxor((macierz),uint8(pi));
+imwrite(pic,'szum.jpg','jpg'); %zapisanie obrazu zakodowanego
 
-for i=round(3/4*h):h
-    for j=round(3/4*b):b
-    y(i,j)=rem(y(i-1,j-1)*a+m,255);
-    end
-end
 
-for i=round(h/2):round(3*h/4)
-    for j=round(b/2):3*round(b/4)
-    y(i,j)=rem(y(i-1,j-1)*a+m,255);
-    end
-end
